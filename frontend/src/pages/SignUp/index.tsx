84b633525f37384e0f435144831ae650a0cb2f7c
jefferson-luis-nascimento <je.luis.nascimento@hotmail.com>
2020-04-30 16:50:57 -0300

    Página de cadastro

---
+++ frontend/src/pages/SignUp/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi';
+
+import logo from '../../assets/logo.svg';
+
+import { Container, Content, Background } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+const SignUp: React.FC = () => {
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <img src={logo} alt="GoBarber" />
+        <form>
+          <h1>Faça seu cadastro</h1>
+          <Input name="name" icon={FiUser} placeholder="Nome" />
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
+          <Button>Entrar</Button>
+        </form>
+        <a href="login">
+          <FiArrowLeft />
+          Voltar para logon
+        </a>
+      </Content>
+    </Container>
+  );
+};
+
+export default SignUp;
