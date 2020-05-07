1c65161fde245db0aeda750fdb0ada1f36cb7fe2
jefferson-luis-nascimento <je.luis.nascimento@hotmail.com>
2020-05-07 18:53:37 -0300

    Testando criação de usuário

---
+++ backend/src/modules/users/services/CreateSessionService.spec.ts
@@ -0,0 +1,11 @@
+import 'reflect-metadata';
+
+import AppError from '@shared/errors/AppError';
+
+describe('CreateSession', () => {
+  it('should be able to create a new session', async () => {});
+
+  it('should not be able to create a session with invalid email', async () => {});
+
+  it('should not be able to create a session with invalid password', async () => {});
+});
---
+++ backend/src/modules/users/services/CreateUserService.spec.ts
@@ -0,0 +1,42 @@
+import 'reflect-metadata';
+
+import AppError from '@shared/errors/AppError';
+
+import CreateUserService from './CreateUserService';
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+
+describe('CreateUser', () => {
+  it('should be able to create a new user', async () => {
+    const fakeUserRepository = new FakeUsersRepository();
+
+    const createUser = new CreateUserService(fakeUserRepository);
+
+    const user = await createUser.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    expect(user).toHaveProperty('id');
+  });
+
+  it('should not be able to create a new user with same email', async () => {
+    const fakeUserRepository = new FakeUsersRepository();
+
+    const createUser = new CreateUserService(fakeUserRepository);
+
+    await createUser.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    expect(
+      createUser.execute({
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
